fd4ddfbc2f0d058ec5cb2376822fda6786b6b5d0
Parth-Sikka <118071430+Parth-Sikka@users.noreply.github.com>
2024-09-12 23:56:23 +0530

    Update Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -32,9 +32,9 @@ export default function Nav() {
                     <nav className="hidden md:block">
                         <ul className="flex gap-10 items-center capitalize font-semibold">
                             <Link href="#Hero">Home</Link>
-                            <Link href="#AboutUs">about us</Link>
-                            <Link href="#Services">services</Link>
-                            <Link href="#Blogs">blog</Link>
+                            <Link href="#AboutUs">About us</Link>
+                            <Link href="#WorkingProcess">Process</Link>
+                            <Link href="#Services">Services</Link>
                         </ul>
                     </nav>
                     <Button
@@ -72,9 +72,8 @@ export default function Nav() {
                         <ul className="flex flex-col items-center gap-6 capitalize font-semibold">
                             <Link href="#Hero">Home</Link>
                             <Link href="#AboutUs">about us</Link>
+                            <Link href="#WorkingProcess">Process</Link>
                             <Link href="#Services">services</Link>
-                            <Link href="#">pricing</Link>
-                            <Link href="#">blog</Link>
                         </ul>
                     </nav>
                 </div>
